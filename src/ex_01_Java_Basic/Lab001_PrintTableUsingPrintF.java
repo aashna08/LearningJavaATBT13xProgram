ef38030bd6a8e0ce9d27f1858e443d879084b433
aashna_gupta <aashna.gupta@alertenterprise.com>
2025-06-10 09:18:35 -0500

    June 10 assignment

---
+++ src/ex_01_Java_Basic/Lab001_PrintTableUsingPrintF.java
@@ -0,0 +1,26 @@
+package ex_01_Java_Basic;
+import java.util.Scanner;
+
+public class Lab001_PrintTableUsingPrintF {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=sc.nextInt();
+        int i=0;
+        System.out.printf("%d * %d = %d%n",num,++i,num*i);
+        System.out.printf("%d * %d = %d%n",num,++i,num*i);
+        System.out.printf("%d * %d = %d%n",num,++i,num*i);
+        System.out.printf("%d * %d = %d%n",num,++i,num*i);
+        System.out.printf("%d * %d = %d%n",num,++i,num*i);
+        System.out.printf("%d * %d = %d%n",num,++i,num*i);
+        System.out.printf("%d * %d = %d%n",num,++i,num*i);
+        System.out.printf("%d * %d = %d%n",num,++i,num*i);
+        System.out.printf("%d * %d = %d%n",num,++i,num*i);
+        System.out.printf("%d * %d = %d%n",num,++i,num*i);
+
+    }
+    }
+
+
+
