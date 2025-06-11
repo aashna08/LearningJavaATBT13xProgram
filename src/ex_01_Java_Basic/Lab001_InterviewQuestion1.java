2cda5d91b17fe732a63f713f1485298eb749c0ec
aashna_gupta <aashna.gupta@alertenterprise.com>
2025-06-11 07:40:25 -0500

    interview question

---
+++ src/ex_01_Java_Basic/Lab001_InterviewQuestion1.java
@@ -0,0 +1,23 @@
+package ex_01_Java_Basic;
+
+public class Lab001_InterviewQuestion1 {
+
+    public static void main(String[] args) {
+        int a =10;
+        int b=20;
+        String first = "Test";
+        String last="Demo";
+        System.out.println(first+last+a+b); //TestDemo1020
+        System.out.println(a+b+first+last); //30TestDemo
+        System.out.println(first+last+(a+b)); //TestDemo30
+        System.out.println(!(10>20)); //true
+        char a1='A';
+        char b1='B';
+        System.out.println(a1); //A
+        System.out.println(a1+b1); //131
+        System.out.println(a1+first); //ATest
+
+
+
+    }
+}
