6c3ee2ba3fad4c9b5314a1ed2bd53e3616ac5dbe
aashna_gupta <aashna.gupta@alertenterprise.com>
2025-06-18 21:50:52 -0500

    Question4

---
+++ src/Test_Questions/Ques004_inc_dec.java
@@ -0,0 +1,13 @@
+package Test_Questions;
+
+public class Ques004_inc_dec {
+    public static void main(String[] args) {
+        int a =10;
+        System.out.println(a++); //Output 10 it will print the value first and then increase a value so value of a is 11
+        System.out.println(++a); //Output 12 value of a is 11 and pre increment first increase the  value and then print it
+
+        int b=20;
+        System.out.println(b--); //20
+        System.out.println(--b); //18
+    }
+}
