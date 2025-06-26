8f0c5caf1e93d7412145e48c04d0fa1d7c8fa608
aashna_gupta <aashna.gupta@alertenterprise.com>
2025-06-26 13:07:50 -0500

    string exercise

---
+++ src/Test_Questions/String002_Compare.java
@@ -0,0 +1,12 @@
+package Test_Questions;
+
+public class String002_Compare {
+    public static void main(String[] args) {
+        String s1="Hello";
+        String s2="hello";
+        String s3="Hello";
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.equalsIgnoreCase(s2));
+
+    }
+}
