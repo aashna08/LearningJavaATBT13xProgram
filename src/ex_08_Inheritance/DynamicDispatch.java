87da5a3cfaf54d4c79dcaeb934a79ce2e3fb3eb4
aashna_gupta <aashna.gupta@alertenterprise.com>
2025-10-29 11:52:57 +0530

    New questions

--- src/ex_08_Inheritance/DynamicDispatch.java
+++ src/ex_08_Inheritance/DynamicDispatch.java
@@ -1,30 +1,24 @@
-//package ex_08_Inheritance;
-//
-//public class DynamicDispatch {
-//    public static void main(String[] args) {
-//        Father f=new Son();
-//        f.f1(); //only common methods can be accessed
-//       // f.s1(); not possible
-//
-//        Son s=new Son();
-//        s.s1();
-//        s.f1();
-//
-//    }
-//
-//    class Father
-//    {
-//        public void f1()
-//        {
-//            System.out.println("Father class method");
-//        }
-//    }
-//
-//    class Son extends Father
-//    {
-//        public void s1()
-//        {
-//            System.out.println("Son class method");
-//        }
-//    }
-//}
+package ex_08_Inheritance;
+class Parent {
+    void display() {
+        System.out.println("Display method in Parent");
+    }
+}
+
+class Child extends Parent {
+    void display() {
+        System.out.println("Display method in Child");
+    }
+
+    void show() {
+        System.out.println("Show method in Child");
+    }
+}
+
+public class DynamicDispatch {
+    public static void main(String[] args) {
+        Parent p = new Child();
+        p.display();   // ✅ valid //calls child method
+        // p.show();   // ❌ error
+    }
+}
